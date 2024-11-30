dee931300f1df6de24df306ef18d457be9938d7f
Pankaj8700 <pankajku.8700@gmail.com>
2024-11-30 23:52:18 +0530

    solve algo problem

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,7 @@ import unittest
 class TestModel(unittest.TestCase):
     def test_model(self):
         model = joblib.load('model/house.pkl')
-        self.assertIsInstance(model, LogisticRegression)
+        self.assertIsInstance(model, LinearRegression)
         self.assertGreaterEqual(len(model.coef_[0]), 8)
 
 if __name__ == '__main__':
